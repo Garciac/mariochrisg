a2cad5412d437bed34b2e29f99eb8edf98c41efc
Chris Garcia <chrisg1245@hotmail.com>
2014-12-01 14:17:23 -0800

    added the trigger in level 1

--- public_html/js/entities/entities.js
+++ public_html/js/entities/entities.js
@@ -14,9 +14,11 @@ game.PlayerEntity = me.Entity.extend({
 
         this.renderable.addAnimation("idle", [3]);
         this.renderable.addAnimation("smallWalk", [8, 9, 10, 11, 12, 13], 80);
-
+        
         this.renderable.setCurrentAnimation("idle");
-                this.body.setVelocity(5, 20);
+        
+        this.body.setVelocity(5, 20);
+        me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
     },
     update: function(delta) {
         if (me.input.isKeyPressed("right")) {
@@ -33,29 +35,30 @@ game.PlayerEntity = me.Entity.extend({
         } else {
             this.renderable.setCurrentAnimation("idle");
         }
-
+         
         this.body.update(delta);
         me.collision.check(this, true, this.collideHandler.bind(this), true);
-        
+
         this._super(me.Entity, "update", [delta]);
         return true;
     },
-    
-    collideHandler: function(response){
-        
+    collideHandler: function(response) {
+
     }
-    
-    });
-    
-    game.LevelTrigger = me.Entity.extend({
-        init: function(x, y, settings){
-      this._super(me.Entity, 'init', [x, y, settings]);
-      this.body.onCollision = this.onCollision.bind(this);
-      this.level = settings.level;
-      },
-     
-    onCollision: function(){
-        this.body.setCollisionMark(me.collision.types.NO_OBJECT);
+
+});
+
+game.LevelTrigger = me.Entity.extend({
+    init: function(x, y, settings) {
+        this._super(me.Entity, 'init', [x, y, settings]);
+        this.body.onCollision = this.onCollision.bind(this);
+        this.level = settings.level;
+        this.xSpawn = settings.xSpawn;
+        this.ySpawn = settings.ySpawn;
+    },
+    onCollision: function() {
+        this.body.setCollisionMask(me.collision.types.NO_OBJECT);
         me.levelDirector.loadLevel(this.level);
+        me.state.current().resetPlayer(this.xSpawn, this.ySpawn);
     }
-    });
+});
--- public_html/js/resources.js
+++ public_html/js/resources.js
@@ -1,33 +1,32 @@
 game.resources = [
+    /* Graphics. 
+     * @example
+     * {name: "example", type:"image", src: "data/img/example.png"},
+     */
+    {name: "background-tiles", type: "image", src: "data/img/background-tiles.png"},
+    {name: "meta-tiles", type: "image", src: "data/img/meta-tiles.png"},
+    {name: "mario", type: "image", src: "data/img/player1.png"},
+    /* Atlases 
+     * @example
+     * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
+     */
 
-	/* Graphics. 
-	 * @example
-	 * {name: "example", type:"image", src: "data/img/example.png"},
-	 */
-{name: "background-tiles", type:"image", src: "data/img/background-tiles.png"},
-{name: "meta-tiles", type:"image", src: "data/img/meta-tiles.png"},
-{name: "mario", type:"image", src: "data/img/player1.png"},
-	/* Atlases 
-	 * @example
-	 * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
-	 */
-		
-	/* Maps. 
-	 * @example
-	 * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
-	 * {name: "example01", type: "tmx", src: "data/map/example01.json"},
- 	 */
-    
-    {name: "ChrisLevel1", type: "tmx", src: "data/map/Chrislevel01.tmx"},
-    {name: "ChrisLevel2", type: "tmx", src: "data/map/Chrislevel02.tmx"}
-        
-	/* Background music. 
-	 * @example
-	 * {name: "example_bgm", type: "audio", src: "data/bgm/"},
-	 */	
+    /* Maps. 
+     * @example
+     * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
+     * {name: "example01", type: "tmx", src: "data/map/example01.json"},
+     */
 
-	/* Sound effects. 
-	 * @example
-	 * {name: "example_sfx", type: "audio", src: "data/sfx/"}
-	 */
+    {name: "ChrisLevel01", type: "tmx", src: "data/map/Chrislevel01.tmx"},
+    {name: "ChrisLevel02", type: "tmx", src: "data/map/Chrislevel02.tmx"}
+
+    /* Background music. 
+     * @example
+     * {name: "example_bgm", type: "audio", src: "data/bgm/"},
+     */
+
+    /* Sound effects. 
+     * @example
+     * {name: "example_sfx", type: "audio", src: "data/sfx/"}
+     */
 ];
