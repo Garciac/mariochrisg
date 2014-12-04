fd3a422671bd5f5c2cd2963f75425fc579a52eae
Chris Garcia <chrisg1245@hotmail.com>
2014-12-04 14:02:27 -0800

    finished title screen

--- public_html/js/screens/title.js
+++ public_html/js/screens/title.js
@@ -3,8 +3,21 @@
 	 *  action to perform on state change
 	 */
 	onResetEvent: function() {	
-		me.game.world.addChild( new me.Sprite (0, 0, me.loader.getImage('title-screen')), 3);
+		me.game.world.addChild( new me.Sprite (0, 0, me.loader.getImage('title-screen')), -10);
                 me.input.bindKey(me.input.KEY.ENTER, "start");
+                
+                me.game.world.addChild(new (me.Renderable.extend({
+                    init: function(){
+                        this._super(me.Renderable, 'init', [510, 30, me.game.viewport.width, me.game.viewport.height]);
+                        this.font = new me.Font ("Arial", 46, "White");
+                    },
+                    
+                    draw: function(renderer){
+                        this.font.draw(renderer.getContext (), "Not Super Mario", 540, 130);
+                        this.font.draw(renderer.getContext (), "Press ENTER to begin to Play!", 250, 530);
+                    }
+                    
+                })));
                 
                 this.handler = me.event.subscribe(me.event.KEYDOWN, function (action,keyCode, edge){
                     if(action === "start"){
@@ -18,6 +31,7 @@
 	 *  action to perform when leaving this screen (state change)
 	 */
 	onDestroyEvent: function() {
-		; // TODO
+             me.input,unbindKey(me.input.KEY.ENTER); 
+             me.event.unsubscribe(this.handler);
 	}
 });
